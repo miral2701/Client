9b96905f111a45d0dc176d629ae1ba346c5567e0
Mira Madani <miramadani2701@gmail.com>
2024-09-13 20:27:02 +0300

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+namespace Client
+{
+    internal class Program
+    {
+        static HubConnection connection;
+        static async Task Main(string[] args)
+        {
+            connection = new HubConnectionBuilder()
+                        .WithUrl("https://localhost:7027/kurs")
+                        .Build();
+            connection.On<string,decimal>("ReceiveMessage", (a,b) =>
+            {
+                
+                    Console.WriteLine($"Message from server: {a}b->{b}");
+                
+               
+            });
+            await connection.StartAsync();
+            Console.WriteLine("Connected to server");
+            Console.ReadLine();
+        }
+
+    }
+}
